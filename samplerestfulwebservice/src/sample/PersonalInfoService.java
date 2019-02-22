8f8eff2dd8de2307a0a7fa70bbb651f462e39ffe
carlo <carlo.paulo.tolentino@gmail.com>
2019-02-22 10:15:08 +0800

    Added a json sample for rest services

--- samplerestfulwebservice/src/sample/PersonalInfoService.java
+++ samplerestfulwebservice/src/sample/PersonalInfoService.java
@@ -2,17 +2,27 @@ package sample;
 
 import java.util.List;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-@Path("/personalinfo")
 
+@Path("/person")
 public class PersonalInfoService {
 	SampleDao sD = new SampleDao();
+	
 	@GET
+	@Path("/getperson")
 	public List<PersonalInfo> getInfo(){
 		return sD.getInfo();
+	}
+	
+	@GET
+	@Path("/getjson")
+	@Produces("application/json")
+	public String getJson(){
+		String pattern = "{ \"name\" : \" %s \"}";
+		return String.format(pattern, "Carlo Paulo Tolentino");
 	}
 
 }
